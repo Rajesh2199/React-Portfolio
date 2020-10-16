35a3a3a75c1d334c19bb9f7055b417ee545b11de
baduraje@isu.edu <baduraje@isu.edu>
2020-10-16 14:12:59 -0600

    added intro section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
+import "materialize-css/dist/css/materialize.min.css";
 
-function Header (){
+
+
+function Intro (){
+
     return (
-        <div className = "header">
-            <h1>This is a header</h1>
+
+            
+        <div className="col s12 m8 offset-m2 l6 offset-l3">
+            <div className="card-panel grey lighten-5 z-depth-1">
+                <div className="row valign-wrapper">
+                <div className="col s2">
+                    <img src= "/image.jpg" alt="mypicture" className="circle responsive-img"></img>
+                </div>
+                <div className="col s10">
+                    <span className="black-text justify">
+                    Hi, I am  Rajesh Baduwal a Full Stack Developer with a Military(U.S Army) and  Accounting background. Thank you for visiting my portfolio. Please take a moment to look at projects I have created. Do not hesitate to send me an email or call me if you have any questions.
+                    <br></br>
+                    <p> <b/>Skills:== HTML, CSS, Bootstrap, JavaScript, APIs, Node.js, Express.js, MySQL, MVC, ORM, MongoDB, Objective Oriented Programming, basic Python (Numpy, Matplotlib, Seaborn) and Java. </p>
+                    </span>
+                    
+                </div>
+            </div>
         </div>
-    )
-}
-
-
-export default Header;
+        </div>
+    )}
+          
+export default Intro;
---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Projects (){
+  return (
+
+   <div>
+     <p>Projects will appear here.</p>
+   </div>
+  )
+}
+
+export default Projects;
