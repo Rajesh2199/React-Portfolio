5673e22b6ca0a6320e50e984bdb295c9183a1427
baduraje@isu.edu <baduraje@isu.edu>
2020-10-30 01:14:52 -0600

    Updates

---
+++ precache-manifest.2a8ec58c0e62bc720340409bdc8f576d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c669f4ee1c4c9beecc7804e573e7964",
+    "url": "/Rajesh2199/React-Portfolio/index.html"
+  },
+  {
+    "revision": "85e189dc9e97ffa57f03",
+    "url": "/Rajesh2199/React-Portfolio/static/css/2.88bd6bfe.chunk.css"
+  },
+  {
+    "revision": "0581540f4523ab80f35b",
+    "url": "/Rajesh2199/React-Portfolio/static/css/main.bb1f5480.chunk.css"
+  },
+  {
+    "revision": "85e189dc9e97ffa57f03",
+    "url": "/Rajesh2199/React-Portfolio/static/js/2.8c6e4557.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Rajesh2199/React-Portfolio/static/js/2.8c6e4557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0581540f4523ab80f35b",
+    "url": "/Rajesh2199/React-Portfolio/static/js/main.fa799c90.chunk.js"
+  },
+  {
+    "revision": "e2f6fecfef9a3b68a33a",
+    "url": "/Rajesh2199/React-Portfolio/static/js/runtime-main.7226d454.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rajesh2199/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
